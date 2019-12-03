fc07b80cea635579cfb3391c10dde6915fdae31f
Gooss <gooss@media-saturn.com>
2019-12-03 15:28:55 +0100

    Background animation + sound done

--- src/logic.js
+++ src/logic.js
@@ -1,36 +1,31 @@
-let music = new Audio("../sounds/game.mp3");
-let intro = new Audio("../sounds/intro.mp3");
+let music = new Audio("./sounds/game.mp3");
+let intro = new Audio("./sounds/intro.mp3");
 
 window.onload = function () {
-    intro.play();
-    document.getElementById('start-button').onclick = function () {
-      intro.pause();
-      music.play();
-      let playfield = new Board(800, 500);
-      playfield.updateCanvas();
-    }
+  intro.play();
+  document.getElementById('start-button').onclick = function () {
+    intro.pause();
+    music.play();
+    let playfield = new Board(800, 500);
+    playfield.scroll();
+  }
 }
 
-
 class Board {
-  constructor(width,height){
+  constructor(width, height) {
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext("2d");
     this.canvas.width = width;
     this.canvas.height = height;
     document.getElementById("game-board").appendChild(this.canvas);
     this.img = new Image();
-    this.img.src = "../img/finished_background.jpg";
+    this.img.src = "./img/finished_background.jpg";
     this.x = 0;
     this.absoluteWidth = 1143;
-    this.speed = -10;
-    this.updateCanvas = this.updateCanvas.bind(this);
+    this.speed = -5;
+    this.scroll = this.scroll.bind(this);
   }
-  move(){
-    this.x += this.speed;
-    this.x %= this.absoluteWidth;
-  }
-  draw(){
+  draw() {
     this.ctx.drawImage(this.img, this.x, 0);
     if (this.x < 0) {
       this.ctx.drawImage(this.img, this.x + this.absoluteWidth, 0);
@@ -38,11 +33,38 @@ class Board {
       this.ctx.drawImage(this.img, this.x - this.absoluteWidth, 0);
     }
   }
-  updateCanvas(){
+  scroll() {
     console.log("update")
-    this.ctx.clearRect(0,0,this.absoluteWidth,this.canvas.height);
-    this.move();
+    this.ctx.clearRect(0, 0, this.absoluteWidth, this.canvas.height);
+    this.x += this.speed;
+    this.x %= this.absoluteWidth;
     this.draw();
-    requestAnimationFrame(this.updateCanvas);
+    requestAnimationFrame(this.scroll);
   }
+}
+
+class MovingObject {
+  constructor(x, y) {
+    console.log("i am :", this)
+  }
+}
+
+class Player extends MovingObject {
+  constructor(x, y) {
+    super(x, y)
+    console.log("i am :", this)
+
+  }
+}
+
+class Asteroids extends MovingObject {
+  constructor(x, y) {
+    super(x, y)
+    console.log("i am :", this)
+
+  }
+}
+
+class Game {
+
 }
